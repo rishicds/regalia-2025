a4a0e2f611442732e5a999f80b8cf1921c3601ec
rishicds <rishi.404@outlook.com>
2025-04-10 18:09:16 +0530

    fix conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import Hero from '@/components/Hero';
+import Sponsors from '@/components/Sponsors/Sponsors';
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <h1>Regalia 2025</h1>
-    </div>
+    <>
+   <Hero/>
+   <Sponsors/>
+   </>
   )
 }
 
